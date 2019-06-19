132688fe01606865abb470df6b575a4179eb65d6
diiceas <forthepost@gmail.com>
2019-06-19 18:06:03 +0300

    update class hierarchy

--- class-method-overriding/src/modules/main.js
+++ class-method-overriding/src/modules/main.js
@@ -1,22 +1,53 @@
 class Person {
+  personName = 'Person';
+
   constructor() {
-    this.doSomething();
+    this.personMethod();
   }
 
-  doSomething() {
+  personMethod() {
     console.log('doSomething class Person');
   }
 }
 
 class Student extends Person {
+  studentName = 'Student';
+
   constructor(firstName, lastName) {
     super();
+    // this.studentMethod();
   }
 
-  doSomething() {
-    super.doSomething();
+  studentMethod() {
+    // super.doSomething();
     console.log('doSomething class Student');
   }
 }
 
-const student = new Student();
+class FrontendStudent extends Student {
+  frontendStudentName = 'FrontendStudent';
+
+  constructor(firstName, lastName) {
+    super();
+    this.frontendStudentMethod();
+  }
+
+  frontendStudentMethod() {
+    // super.doSomething();
+    console.log('doSomething class Frontend Student');
+  }
+}
+
+const frontendStudent = new FrontendStudent();
+console.log('TCL: frontendStudent', frontendStudent);
+
+// function Person() {}
+// Person.prototype.personMethod = function() {};
+// function Student() {}
+
+// Student.prototype = Object.create(Person.prototype);
+// Student.prototype.constructor = Student;
+
+// Student.prototype.studentMethod = function() {};
+// const student = new Student();
+// console.log('TCL: student', student);
